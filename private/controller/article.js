acd61d65f5ef5a2598c80d33a3f3fcdf26d3ee05
abelardogg <abelardogy@gmail.com>
2020-02-22 21:02:50 -0700

    read one article

--- private/controller/article.js
+++ private/controller/article.js
@@ -2,17 +2,39 @@ const express = require('express');
 const router = express.Router();
 const articleRepository = require('../repository/articleRepository.js');
 
+router.get('/read/:articleId', async function(req, res, next){
+    const articleId = Number(req.params.articleId);
+    try {
+        let article = await articleRepository.findArticle(articleId);
+        console.log(article);
+
+        // TODO create validation for hidden articles
+
+        res.render('article/read', article);
+    } catch (error) {
+        console.error(error);
+        res.send({error: 'something went wrong'});
+    }
+    
+});
+
 router.get('/create', function(req, res, next){
     res.render('article/create');
 });
 
 router.post('/create', async function(req, res, next){
+    const articleId = new Date().getTime();
+    const articleDate = Date();
+
     let article = {
+        articleId: articleId,
         title: req.body.title,
+        shortDescription: req.body.shortDescription,
         author: req.body.author,
         content: req.body.content,
         tags: 'test',
-        date: Date()
+        date: articleDate,
+        hidden: 'false'
     };
 
     try{
--- private/repository/articleRepository.js
+++ private/repository/articleRepository.js
@@ -1,26 +1,41 @@
 const mongoose = require('mongoose');
 
 let articleSchema = new mongoose.Schema({
+    articleId: Number,
     title: String,
     shortDescription: String,
     author: String,
     content: String,
     tags: String,
     date: String,
+    hidden: String
 });
 
-function createArticle(blogContent){
+function createArticle(blogContent) {
     let articleModel = mongoose.model('articles', articleSchema);
     let article = new articleModel(blogContent);
 
-    article.save(function(err, article){
-        if(err){
+    article.save(function (err, article) {
+        if (err) {
             return console.log(article, err);
         }
     });
+}
 
+function findArticle(articleId) {
+    let articleModel = mongoose.model('articles', articleSchema);
+
+    return articleModel.findOne({ articleId: articleId },
+        function (err, response) {
+            if (err) {
+                return console.log(response, err);
+            }
+            console.log(response);
+        })
+        .exec();
 }
 
 module.exports = {
-    createArticle
+    createArticle,
+    findArticle
 }
